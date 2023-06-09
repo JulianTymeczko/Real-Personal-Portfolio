b0efa8903a8f2146f06e9bb6cccb32f3bff2892f
Julian Houle-T <jhouletymeczko@gmail.com>
2023-06-09 01:33:02 -0400

    finisheed

---
+++ assets/scripts/changeDescription.js
@@ -0,0 +1,61 @@
+const changeDescription = (nameCard, text) => {
+    let infoSkills = document.querySelector('#info-skills')
+    let cardName = document.querySelector('.card-name')
+
+    document.querySelector(nameCard).addEventListener('mouseover', () => {
+        infoSkills.innerHTML = text
+        if(nameCard === '.html') {
+            cardName.innerHTML = ' Html '
+        } 
+        if(nameCard === '.css') {
+            cardName.innerHTML = ' Css '
+        }
+        if(nameCard === '.js') {
+            cardName.innerHTML = ' JavaScript '
+        }
+        if(nameCard === '.git') {
+            cardName.innerHTML = ' Git '
+        } 
+        if(nameCard === '.responsive') {
+            cardName.innerHTML = ' Responsiveness '
+        } 
+        if(nameCard === '.accessibility') {
+            cardName.innerHTML = ' Accessibility '
+        } 
+    })
+
+    document.querySelector(nameCard).addEventListener('mouseout', () => {
+        infoSkills.innerHTML = '* place your cursor over one of these categories *'
+        cardName.innerHTML = ""
+    })
+}
+
+changeDescription(
+    '.html',
+    'Using semantic elements to structure layouts, forms, tables, and many different types of curcial basics of web development.'
+)
+
+changeDescription(
+    '.css',
+    'Using box model, CSS grid, flexbox, pseudo-classes, animations and Bootstrap.'
+)
+
+changeDescription(
+    '.js',
+    'Traversing the DOM, using libraries like JQuery, medthods and events with functions.'
+)
+
+changeDescription(
+    '.git',
+    'Version control and using Git commands.'
+)
+
+changeDescription(
+    '.responsive',
+    'Understanding responsive design, breakpoints and ensuring flexibility of the elements on a page.'   
+)
+
+changeDescription(
+    '.accessibility',
+    'Best practices; such as semantic elements and using the alt attribute for screen readers and for boosting the SEO.'   
+)
---
+++ assets/scripts/showMore.js
@@ -0,0 +1,20 @@
+const button = document.querySelector('.show-more')
+const rowProjectsTwo = document.querySelector('.row-2')
+
+rowProjectsTwo.style.display = 'none'
+
+
+const showMore = () => {
+    if(rowProjectsTwo.style.display === 'flex') {
+        rowProjectsTwo.style.display = 'none'
+        button.innerHTML = 'View more'
+    } else if(rowProjectsTwo.style.display === 'none') {
+        rowProjectsTwo.style.display = 'flex'
+        button.innerHTML = 'View less'
+    }
+}
+
+
+
+
+button.addEventListener('click', showMore)
